d8836122ff3b20ca5c16b34ecc5941be09744380
Skyler Manzanares <skymanzanares@hotmail.com>
2021-10-09 17:12:19 -0700

    Re-add commented out particles between aether relays.

--- src/main/java/com/smanzana/nostrumaetheria/blocks/tiles/AetherRelayEntity.java
+++ src/main/java/com/smanzana/nostrumaetheria/blocks/tiles/AetherRelayEntity.java
@@ -8,6 +8,8 @@ import com.smanzana.nostrumaetheria.api.component.OptionalAetherHandlerComponent
 import com.smanzana.nostrumaetheria.component.AetherRelayComponent;
 import com.smanzana.nostrumaetheria.component.AetherRelayComponent.AetherRelayListener;
 import com.smanzana.nostrummagica.NostrumMagica;
+import com.smanzana.nostrummagica.client.particles.NostrumParticles;
+import com.smanzana.nostrummagica.client.particles.NostrumParticles.SpawnParams;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
@@ -15,6 +17,7 @@ import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class AetherRelayEntity extends NativeAetherTickingTileEntity implements AetherRelayListener {
@@ -161,23 +164,23 @@ public class AetherRelayEntity extends NativeAetherTickingTileEntity implements 
 				return;
 			}
 			
-//			final int color;
-//			//if (relay.isAetherActive()) {
-//				color = 0x80D4CF80;
-//			//} else {
-//			//	color = 0x80D3D3CD;
-//			//}
-//			
-//			for (BlockPos dest : links) {
-//				if (NostrumMagica.rand.nextBoolean() && NostrumMagica.rand.nextBoolean()) {
-////					NostrumParticles.GLOW_ORB.spawn(world, new SpawnParams(
-////							1, pos.getX() + .5, pos.getY() + .5, pos.getZ() + .5, 0, 20 * 1, 10, new Vec3d(dest.getX() + .5, dest.getY() + .5, dest.getZ() + .5)
-////						).color(color));
-//					
-//					//int count, double spawnX, double spawnY, double spawnZ, double spawnJitterRadius, int lifetime, int lifetimeJitter,
-//					//Vec3d targetPos
-//				}
-//			}
+			final int color;
+			//if (relay.isAetherActive()) {
+				color = 0x80D4CF80;
+			//} else {
+			//	color = 0x80D3D3CD;
+			//}
+			
+			for (BlockPos dest : links) {
+				if (NostrumMagica.rand.nextBoolean() && NostrumMagica.rand.nextBoolean()) {
+					NostrumParticles.FILLED_ORB.spawn(world, new SpawnParams(
+							1, pos.getX() + .5, pos.getY() + .5, pos.getZ() + .5, 0, 20 * 1, 10, new Vec3d(dest.getX() + .5, dest.getY() + .5, dest.getZ() + .5)
+						).color(color));
+					
+					//int count, double spawnX, double spawnY, double spawnZ, double spawnJitterRadius, int lifetime, int lifetimeJitter,
+					//Vec3d targetPos
+				}
+			}
 		}
 		
 		@Override
@@ -186,7 +189,7 @@ public class AetherRelayEntity extends NativeAetherTickingTileEntity implements 
 			
 			if (this.world != null && this.world.isRemote) {
 				if (ticksExisted > idleTicks) {
-					idleTicks = ticksExisted + NostrumMagica.rand.nextInt(40) + 10;
+					idleTicks = ticksExisted + NostrumMagica.rand.nextInt(120) + 60;
 					idleVisualTick();
 				}
 			}
