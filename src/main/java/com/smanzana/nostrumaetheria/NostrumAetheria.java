6d43942ae33d1122f24681c3cca0f28f21633f2a
Skyler Manzanares <skymanzanares@hotmail.com>
2023-01-15 11:53:28 -0800

    Version bump from pre vious change

--- src/main/java/com/smanzana/nostrumaetheria/NostrumAetheria.java
+++ src/main/java/com/smanzana/nostrumaetheria/NostrumAetheria.java
@@ -34,7 +34,7 @@ dependencies="required-after:" + NostrumMagica.MODID + "@[" + NostrumMagica.VERS
 public class NostrumAetheria
 {
     public static final String MODID = "nostrumaetheria";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1.3";
     public static NostrumAetheria instance;
     @SidedProxy(clientSide="com.smanzana.nostrumaetheria.proxy.ClientProxy", serverSide="com.smanzana.nostrumaetheria.proxy.CommonProxy")
     public static CommonProxy proxy;
